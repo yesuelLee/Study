5a4c5deb50f99fb944cc1a5b67226cb4dc58a9ab
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-10-21 07:59:41 +0900

    Update : Practice Eight Two Day : 211021

---
+++ Java/Support/src/Practice/PracticeEightTwoMain.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class PracticeEightTwoMain {
+
+		public static void main(String[] args) {
+			PracticeEightTwo praEight = new PracticeEightTwo();
+			praEight.run();
+		}
+
+	
+}
