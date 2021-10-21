c67fd02aa42a7b634bfee7114350f9d733dfa0f1
yeseullee <92432931+yesuelLee@users.noreply.github.com>
2021-10-21 23:23:38 +0900

    Add files via upload

---
+++ Java/PracticeTen.java
@@ -0,0 +1,28 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class PracticeTen {
+
+	private int i,j;
+	
+	public void run() {
+		for(i=0;i<3;i++) {
+			for(j=2;j>i;j--) {
+				System.out.print(" ");
+			}
+			for(j=0;j<2*i+1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(i=0;i<3;i++) {
+			for(j=-1;j<i;j++) {
+				System.out.print(" ");
+			}for(j=3;j>2*i;j--) {
+				System.out.print("*");
+			}System.out.println();
+		}
+		
+	}
+}
---
+++ Java/PracticeTenMain.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class PracticeTenMain {
+
+	public static void main(String[] args) {
+		PracticeTen praTen = new PracticeTen();
+		praTen.run();
+	}
+
+}
