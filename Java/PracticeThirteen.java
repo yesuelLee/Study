7299dc165c0e0a2b3f0e2fbfb7d5936c007e6075
yeseullee <92432931+yesuelLee@users.noreply.github.com>
2021-10-23 19:13:43 +0900

    Add files via upload

---
+++ Java/PracticeThirteen.java
@@ -0,0 +1,58 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class PracticeThirteen {
+
+	private Scanner sc ;
+	private int i,j,k,N,s,u;
+	
+	
+	
+	public void run() {
+		System.out.println("숫자를 입력해주세요.");
+		sc=new Scanner(System.in);
+		N=sc.nextInt();
+		i=N/10;
+		j=N%10;
+		if((0<=N)&&(N<=99)) {
+			if(N<10) {
+				while(s<4) {
+					k=i+j;
+					System.out.println(i+"+"+j+"="+k);
+					k=(j*10)+(k%10);
+					i=k/10;
+					j=k%10;
+				s++;	
+				}
+				}
+			
+			
+		}else {
+			System.out.println("0보다 크거나 같은 숫자, 99보다 작거나 같은 숫자만 입력해주세요.");
+		}
+		
+	}
+	
+	public void run2() {
+		System.out.println("숫자를 입력해주세요.");
+		sc=new Scanner(System.in);
+		N=sc.nextInt();
+		
+		i=N/10; j=N%10; 
+		if((0<=N)&&(N<=99)) {
+				for(s=0; s<4; s++) {
+					k=i+j;
+					System.out.println(i+"+"+j+"="+k);
+					k=(j*10)+(k%10);
+					i=k/10;
+					j=k%10;
+		   
+				}
+	
+		}
+	}
+	
+	
+	
+}
---
+++ Java/PracticeThirteenMain.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class PracticeThirteenMain {
+
+	public static void main(String[] args) {
+		PracticeThirteen praTir = new PracticeThirteen();
+		praTir.run();
+	}
+
+}
