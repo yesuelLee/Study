758d7f8c0492dc0b757dcffd3fad8f15ad42afbc
yeseullee <92432931+yesuelLee@users.noreply.github.com>
2021-11-06 23:34:15 +0900

    Update practiceThree.java

--- Java/practiceThree.java
+++ Java/practiceThree.java
@@ -1,5 +1,33 @@
-package practice;
+package Practice;
+
+import java.util.Scanner;
 
 public class practiceThree {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N;
+		int X;
+		
+		   
+		System.out.println("수열의 갯수를 입력하세요.");
+		N =sc.nextInt();
+		
+	    System.out.println("정수를 입력하세요.");
+	    X=sc.nextInt();
+	    
+	    int[] A = new int[N];
+	    
+	    System.out.println("원하는 숫자를 입력하세요.");
+	    for(int i=0; i<N; i++) {
+	    	A[i]=sc.nextInt();
+	    }
+	    
+	    for(int j=0; j<N; j++) {
+	    	if(A[j]<X) {
+	    		System.out.println("입력하신 숫자중에 "+X+"보다 작은 숫자는"+(A[j])+""+"입니다.");
+	    	}
+	    }
+	   
 
+ }
 }
