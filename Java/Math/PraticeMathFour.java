00de537c75c7d4bfe90058004663d8539f0c8d11
yeseullee <92432931+yesuelLee@users.noreply.github.com>
2021-11-20 20:12:36 +0900

    Add files via upload

---
+++ Java/Math/PraticeMathFour.java
@@ -0,0 +1,63 @@
+package Practice;
+import java.math.BigInteger;
+//새로 배운 내용
+//1.BigInteger : 문자열(사칙연산 안됨)
+//덧셈하려면 A+B는 ->A.add(B)); 처럼 사용해야 함.
+
+import java.util.Scanner;
+
+
+public class PraticeMathFour{
+
+	private Scanner sc;
+	int N,count;
+	
+	public void run() {
+		sc = new Scanner(System.in);
+		System.out.println("숫자를 입력해주세요.");
+		N=sc.nextInt();
+		count=0;
+		
+		while(true) {
+			if(N%5==0) {
+				count+=N/5;
+				break;
+			}
+			N-=3;
+			count++;
+			if(N<0) {
+				count=-1;
+				break;
+			}
+		}System.out.println("상근이가 배달하는 봉지의 최소 개수는 "+count+"개 입니다.");
+	
+	}//run-end
+
+
+	
+		
+	
+
+
+}//class-end
+
+/*
+문제
+상근이는 요즘 설탕공장에서 설탕을 배달하고 있다.
+ 상근이는 지금 사탕가게에 설탕을 정확하게 N킬로그램을 배달해야 한다. 
+ 설탕공장에서 만드는 설탕은 봉지에 담겨져 있다. 봉지는 3킬로그램 봉지와 5킬로그램 봉지가 있다.
+
+상근이는 귀찮기 때문에, 최대한 적은 봉지를 들고 가려고 한다. 
+예를 들어, 18킬로그램 설탕을 배달해야 할 때, 3킬로그램 봉지 6개를 가져가도 되지만, 
+5킬로그램 3개와 3킬로그램 1개를 배달하면, 더 적은 개수의 봉지를 배달할 수 있다.
+
+상근이가 설탕을 정확하게 N킬로그램 배달해야 할 때, 
+봉지 몇 개를 가져가면 되는지 그 수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N이 주어진다. (3 ≤ N ≤ 5000)
+
+출력
+상근이가 배달하는 봉지의 최소 개수를 출력한다. 만약, 정확하게 N킬로그램을 만들 수 없다면 -1을 출력한다.
+ 
+ */
