33ba7123720c5c3659c468c09217023289b170b9
yeseullee <92432931+yesuelLee@users.noreply.github.com>
2021-12-16 08:57:44 +0900

    Add files via upload

---
+++ Java/For/PraciticeFor2Nine.java
@@ -0,0 +1,60 @@
+
+/*
+ 주어진 숫자까지 나열
+ 12월 15일 백신 3차 접종으로 못함...매우 슬픔 ㅠ_ㅠ
+*/
+
+
+package Practice;
+
+
+import java.nio.IntBuffer;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class PraciticeFor2Nine{
+	int i,j,a,b,c;
+	public Scanner sc;
+	
+		
+		public void run(){
+			sc=new Scanner(System.in);
+			System.out.println("몇까지의 숫자를 출력하실건가요?");
+			a=sc.nextInt();
+			
+			
+				for(i=1;i<=a;i++) {
+					System.out.print(i+" ");
+					}
+				}
+	
+			}
+			
+		
+
+
+		
+		
+		
+		
+
+/*
+문제 설명
+어떤 수 n을 입력으로 받아 1부터 n까지의 숫자를 출력하시오.
+
+입력
+n이 입력으로 들어온다. (1 <= n <= 100000)
+
+출력
+1부터 n까지 공백으로 띄워 출력한다.
+
+입력 예시   
+5
+
+출력 예시
+1 2 3 4 5 
+
+ */
+ 
+
