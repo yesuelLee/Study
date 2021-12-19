8d2c6ddcffc5823edbd4196dd0471b773b60b4dd
yeseullee <92432931+yesuelLee@users.noreply.github.com>
2021-12-19 15:35:28 +0900

    Add files via upload

---
+++ Java/01.Scanner/PraciticeScannerOne.java
@@ -0,0 +1,51 @@
+
+/*
+주어진 숫자 전부 곱하기
+*/
+
+
+package Practice;
+
+
+import java.nio.IntBuffer;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class PraciticeScannerOne{
+	int a,b,c,d;
+	public Scanner sc;
+	
+		
+		public void run(){
+			sc=new Scanner(System.in);
+			System.out.println("곱하고자 하는 숫자를 차례대로 입력해주세요.");
+			a=sc.nextInt();
+			b=sc.nextInt();
+			c=sc.nextInt();
+			d=sc.nextInt();
+			
+			System.out.println(a*b*c*d);
+		}
+			
+			
+
+
+		
+
+		
+}
+		
+
+
+		
+		
+		
+		
+
+/*
+ 숫자 4개를 입력받아서 모두 곱한 숫자 출력하기.
+
+ */
+ 
+
