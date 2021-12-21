f08c3dbfdd76720e4e3a40ceacd811aa4b9f9de9
yeseullee <92432931+yesuelLee@users.noreply.github.com>
2021-12-21 14:37:33 +0900

    Add files via upload

---
+++ Java/PraciticeStringLove.java
@@ -0,0 +1,62 @@
+/*
+love입력시 I Love you 출력하기!
+equals 비교 연산자
+*/
+
+
+package Practice;
+
+
+import java.util.Scanner;
+
+public class PraciticeStringLove{
+	private Scanner sc ;
+	private String str;
+	
+	
+	
+	public void run() {
+		sc = new Scanner(System.in);
+		System.out.println("문자를 입력해주세요.");
+		str=sc.nextLine();
+		
+		if(str.equals("love")) {
+			System.out.println("I Love You");
+		}else {
+			System.out.println("love외에는 입력받고싶지않습니다만?");
+		}
+		
+
+
+	}
+
+		
+}
+		
+
+
+		
+		
+		
+		
+
+/*
+ 문제 설명
+영단어 하나가 입력된다.
+
+그 단어가 love이면 I love you. 를 출력하시오.
+
+입력
+영어 단어 하나가 입력된다.
+
+출력
+love가 입력되면 I love you.를 출력하시오. 만약 다른 단어가 입력되면 "love외에는 입력받고싶지않습니다만?" 출력.
+
+입력 예시   
+love
+
+출력 예시
+I love you.
+ */
+ 
+
