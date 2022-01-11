e188b82ec47d281fa7183c058eb2ee5bec8202c4
yeseullee <92432931+yesuelLee@users.noreply.github.com>
2022-01-11 23:07:07 +0900

    Update PracitceStringNine.java

--- Java/String/PracitceStringNine.java
+++ Java/String/PracitceStringNine.java
@@ -3,7 +3,7 @@ replaceAll란?
 replaceAll() 함수는 대상 문자열을 원하는 문자값으로 변환하는 함수이다.
 첫번째 매개변수는 변환하고자 하는 대상이 될 문자열
 두번째 매개변수는 변환할 문자 값
-
+--------------------------------------------------------------
 replace와 replaceAll차이
 String allTest = "aaabbbvccacfgdracabtghd"; 
 System.out.println( allTest.replace("ab","0") ); // aa0bbvccacfgdrac0tghd 
